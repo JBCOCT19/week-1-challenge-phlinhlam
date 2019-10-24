c3ea1937fbea68bf92c07ab3234e81defa20f422
GBTC407013UR <GBTC407013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-24 13:40:22 -0400

    user choice or random number commit

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -3,26 +3,52 @@ import java.util.Random;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-       // int userInput;
-        Random rand = new Random();
-        //Part1
-//        Scanner kb = new Scanner(System.in);
-//        System.out.println("Enter a number");
-//        userInput = kb.nextInt();
-        int randomNum = 1 + rand.nextInt((250-1)+1);
+        int userInput;
+        String yesNo;
         boolean isPrime = true;
-        if (randomNum > 2) {
-            for (int i = 2; i <= (randomNum / 2); i++) {
-                if (randomNum % i == 0) {
-                    isPrime = false;
-                    System.out.println(randomNum + " is not a prime");
-                    break;
+        Random rand = new Random();
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Would you like to generate a random number or input a number? (R/I)");
+        yesNo = kb.nextLine();
+        int randomNum = 1 + rand.nextInt((250-1)+1);
+        switch (yesNo)
+        {
+            case "r":
+            case "R":
+                System.out.println("Generating random number....");
+                if (randomNum > 2) {
+                    for (int i = 2; i <= (randomNum / 2); i++) {
+                        if (randomNum % i == 0) {
+                            isPrime = false;
+                            System.out.println(randomNum + " is not a prime");
+                            break;
+                        }
+                    }//end for
+                }//end if
+                if (isPrime == true) {
+                    System.out.println(randomNum + " is a prime");//run no matter what
                 }
-            }//end for
-        }//end if
-        if (isPrime == true) {
-            System.out.println(randomNum + " is a prime");//run no matter what
+                break;//break out of case r
+            case "i":
+            case "I":
+                System.out.println("Please enter a number");
+                userInput = kb.nextInt();
+                if (userInput  > 2) {
+                    for (int i = 2; i <= (userInput / 2); i++) {
+                        if (userInput % i == 0) {
+                            isPrime = false;
+                            System.out.println(userInput + " is not a prime");
+                            break;
+                        }
+                    }//end for
+                }//end if
+                if (isPrime == true) {
+                    System.out.println(userInput + " is a prime");//run no matter what
+                }
+                break;//break out of case i
         }
+
 
     }//end main
 }//end class

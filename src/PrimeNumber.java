543af66af981a06a0febe49bb6c5df6e2a9b307d
GBTC407013UR <GBTC407013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-24 13:27:15 -0400

    second part commit

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int userInput;
-        Scanner kb = new Scanner(System.in);
-        System.out.println("Enter a number");
-        userInput = kb.nextInt();
-
+       // int userInput;
+        Random rand = new Random();
+        //Part1
+//        Scanner kb = new Scanner(System.in);
+//        System.out.println("Enter a number");
+//        userInput = kb.nextInt();
+        int randomNum = 1 + rand.nextInt((250-1)+1);
         boolean isPrime = true;
-        if (userInput > 2) {
-            for (int i = 2; i <= (userInput / 2); i++) {
-                if (userInput % i == 0) {
+        if (randomNum > 2) {
+            for (int i = 2; i <= (randomNum / 2); i++) {
+                if (randomNum % i == 0) {
                     isPrime = false;
-                    System.out.println(userInput + " is not a prime");
+                    System.out.println(randomNum + " is not a prime");
                     break;
                 }
             }//end for
         }//end if
         if (isPrime == true) {
-            System.out.println(userInput + " is a prime");//run no matter what
+            System.out.println(randomNum + " is a prime");//run no matter what
         }
 
     }//end main
